5e07d6ceb677f5a2c5d2808ec79c02dedbd0c38c
bordon.leandro <leandro.bordon@balloon-group.com>
2024-07-12 15:03:24 -0300

    :sparkles: qa id client url

--- src/utils/fetchUserId.ts
+++ src/utils/fetchUserId.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+//const VITE_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const VITE_USER_ID = import.meta.env.VITE_USER_ID;
 
 export default function UserID() {
@@ -7,10 +8,13 @@ export default function UserID() {
   const [error, setError] = useState(null);
     useEffect(() => {
   const fetchData = async () => {
+   // const url = VITE_BASE_URL + VITE_USER_ID;
+  //  console.log(url)
     try {
       const response = await fetch(VITE_USER_ID);
       const jsonData = await response.json();
      jsonData.id === null ? setId("Usuario sin ID") : setId(jsonData.id);
+     console.log("ID de Usuario",jsonData)
     } catch (error) {
       setError(error.message);
     }
